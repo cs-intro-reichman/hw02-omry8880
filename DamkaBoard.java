9be53552db9dee25e41660eeb2c2c78fbcb89762
Omry Maor <omry.maor@runi.ac.il>
2023-12-31 17:08:17 +0200

    Finished OneOfEach

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,20 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int lines = Integer.parseInt(args[0]);
+
+		for (int i = 0; i < lines; i++) {
+			for (int j = 0; j < lines; j++) {
+				if (i % 2 == 0) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print(" *");
+				}
+			}
+			if (i != lines-1) {
+				System.out.println("");
+			}
+		}
 	}
 }
--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,24 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		double child;
+		int count = 0;
+
+		boolean girl = false;
+		boolean boy = false;
+
+		while (!(girl && boy)) {
+			count++;
+			child = Math.random();
+			if (child > 0.5) {
+				girl = true;
+				System.out.print("g ");
+			} else { 
+				boy = true;
+				System.out.print("b ");
+			}
+		}
+
+		System.out.println("\nYou made it... and you now have " + count + " children.");
 	}
 }
